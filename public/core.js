9a1ec7c348440e824b54d389b8d3cfc1d8270f90
Kira Pilot <kira@mavrck.co>
2017-12-26 18:26:04 -0500

    installed angularjs-stellar

--- public/core.js
+++ public/core.js
@@ -1,6 +1,8 @@
-var app = angular.module('pilotPortfolio', []);
+var app = angular.module('pilotPortfolio', ['stellar.directives']);
 
-app.controller("mainCtrl", function($scope) {
+app.controller("mainCtrl", function($scope, stellar) {
+  stellar.against(window);
+
 });
 
 
